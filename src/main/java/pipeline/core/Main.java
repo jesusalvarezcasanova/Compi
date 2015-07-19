bc9e29d1de6867f150b0edf4a0e65de62be51e1c
jesusalvarezcasanova <jesus.alvarez.casanova@outlook.com>
2015-07-20 01:21:23 +0200

    new DOMparsing and use the absolute path of the pipeline.xsd

--- src/main/java/pipeline/core/Main.java
+++ src/main/java/pipeline/core/Main.java
@@ -1,49 +1,41 @@
 package pipeline.core;
 
-import java.io.IOException;
-
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
+import java.io.File;
+import java.net.URL;
 
 import pipeline.validation.DOMparsing;
 
 
 
 public class Main{
-	
-	public static void main(String args[]){
-		
-		//comprobamos si el fichero XML valida con el fichero XSD
-		System.out.println(args[0]);
 
-		try {
-			Document doc = DOMparsing.loadAndValidateWithExternalXSD(args[0],args[1]);
-//			System.out.println(doc.getDocumentElement());
-		} catch (ParserConfigurationException | SAXException | IOException e) {
-			System.err.println("Error en la validacion del fichero XML");
-			e.printStackTrace();
-		}
-		
-		
-//		int threadNumber = Integer.parseInt(args[0]);
-//		ExecutorService eS = Executors.newFixedThreadPool(threadNumber);
-//		Resolver paramResolver = new Resolver();
-//		synchronized (this) {
-//			
-//		}
-//		
-//		Future<?> f = eS.submit();
-//		try {
-//			f.get();
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (ExecutionException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+	public static void main(String args[]){
+
+		//comprobamos si el fichero XML valida con el fichero XSD
+		URL xsdPath = Thread.currentThread().getContextClassLoader().getResource("xsd/pipeline.xsd");
+
+		boolean doc = DOMparsing.validateXMLSchema(args[0], xsdPath.getPath());
+
+		System.out.println(doc);
+
+
+		//		int threadNumber = Integer.parseInt(args[0]);
+		//		ExecutorService eS = Executors.newFixedThreadPool(threadNumber);
+		//		Resolver paramResolver = new Resolver();
+		//		synchronized (this) {
+		//			
+		//		}
+		//		
+		//		Future<?> f = eS.submit();
+		//		try {
+		//			f.get();
+		//		} catch (InterruptedException e) {
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//		} catch (ExecutionException e) {
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//		}
 	}
-	
+
 }
--- src/main/java/pipeline/validation/DOMparsing.java
+++ src/main/java/pipeline/validation/DOMparsing.java
@@ -2,7 +2,6 @@ package pipeline.validation;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.StringReader;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
@@ -11,32 +10,25 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 
 import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class DOMparsing {
-	
-	public static Document loadAndValidateWithExternalXSD(String documentPath, String schemaPath) 
-			throws ParserConfigurationException, SAXException, IOException {
-		// Construcción del schema
-		SchemaFactory schemaFactory = 
-				SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-		Schema schema = schemaFactory.newSchema(new File(schemaPath));
 
-		// Construcción del parser del documento. Se establece el esquema y se activa
-		// la validación y comprobación de namespaces
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		factory.setValidating(false);
-		factory.setNamespaceAware(true);
-		factory.setSchema(schema);
-
-		// Se añade el manejador de errores
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		builder.setErrorHandler(new SimpleErrorHandler());
-
-		return builder.parse(new File(documentPath));
+	public static boolean validateXMLSchema(String xmlPath, String xsdPath){
+		try {
+			SchemaFactory factory = 
+					SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+			Schema schema = factory.newSchema(new File(xsdPath));
+			Validator validator = schema.newValidator();
+			//validator.setErrorHandler(new SimpleErrorHandler());
+			validator.validate(new StreamSource(new File(xmlPath)));
+		} catch (IOException | SAXException e) {
+			System.out.println("Exception: "+e.getMessage());
+			return false;
+		}
+		return true;
 	}
-
 }
